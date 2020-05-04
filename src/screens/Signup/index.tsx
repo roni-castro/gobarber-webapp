651bfc3b8fe2374e648baf1a56f0371bc908cce0
Roni <roni.c.castro@gmail.com>
2020-05-04 00:11:26 -0300

    Use unform

--- src/screens/Signup/index.tsx
+++ src/screens/Signup/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import Logo from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import { Background, Container, Content } from './styles';
 
+interface InputData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const Signup: React.FC = () => {
+  const formRef = useRef(null);
+
+  const handleSubmit = useCallback((data: InputData) => {
+    console.log(data);
+  }, []);
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={Logo} alt="GoBarber logo" />
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -21,8 +34,8 @@ const Signup: React.FC = () => {
             type="password"
             placeholder="Senha"
           />
-          <Button>Cadastrar</Button>
-        </form>
+          <Button type="submit">Cadastrar</Button>
+        </Form>
         <a href="/login">
           <FiArrowLeft />
           Voltar para login
