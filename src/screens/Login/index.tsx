995ab65ef53113b6264d7589f7f30a5139b3dd95
Roni <roni.c.castro@gmail.com>
2020-05-08 01:56:21 -0300

    Add form validation to login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,16 +1,44 @@
-import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-import { Background, Container, Content } from './styles';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import React, { useCallback, useRef } from 'react';
+import { FiLock, FiLogIn, FiMail } from 'react-icons/fi';
+import * as Yup from 'yup';
 import Logo from '../../assets/logo.svg';
-import Input from '../../components/Input';
 import Button from '../../components/Button';
+import Input from '../../components/Input';
+import { Background, Container, Content } from './styles';
+import { mapValidationErrorToErrorObject } from '../../utils/errorObjectMapper';
+
+interface LoginInputData {
+  email: string;
+  password: string;
+}
 
 const Login: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: LoginInputData) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail é obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha é obrigatória'),
+      });
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        formRef.current?.setErrors(mapValidationErrorToErrorObject(error));
+      }
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={Logo} alt="GoBarber logo" />
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
@@ -19,9 +47,9 @@ const Login: React.FC = () => {
             type="password"
             placeholder="Senha"
           />
-          <Button>Entrar</Button>
+          <Button type="submit">Entrar</Button>
           <a href="/forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
         <a href="/signup">
           <FiLogIn />
           Criar conta
