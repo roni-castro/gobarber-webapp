2e7b21f77c6fb186820635f7e6d78b8c387793ff
Roni <roni.c.castro@gmail.com>
2020-05-10 01:38:46 -0300

    Create custom route

---
+++ src/routes/CustomRoute.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+  Redirect,
+} from 'react-router-dom';
+import AuthContext from '../hooks/AuthContext';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const CustomRoute: React.FC<RouteProps> = ({
+  component: Component,
+  isPrivate = false,
+  ...rest
+}) => {
+  const { auth } = AuthContext.useAuth();
+  const isLoggedIn = !!auth.token;
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === isLoggedIn ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default CustomRoute;
