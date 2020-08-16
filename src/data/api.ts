5c3c5540a2a12bffecbc264764c9a51d314c1df7
Roni <roni.c.castro@gmail.com>
2020-08-16 19:44:35 -0300

    Use base url from env

--- src/data/api.ts
+++ src/data/api.ts
@@ -3,7 +3,7 @@ import { getStorageItem } from '../utils/storage';
 import { TOKEN } from './auth/authStorageConstants';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use(
